4b10e74f5e93eee417d8570c155769c21a60671a
Hiroshi Urabe <mail@torounit.com>
2015-08-03 17:55:30 +0900

    wp scaffold plugin multisite-sample-plugin

---
+++ multisite-sample-plugin.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Multisite-sample-plugin
+Version: 0.1-alpha
+Description: PLUGIN DESCRIPTION HERE
+Author: YOUR NAME HERE
+Author URI: YOUR SITE HERE
+Plugin URI: PLUGIN SITE HERE
+Text Domain: multisite-sample-plugin
+Domain Path: /languages
+*/
+
